4d98bc2bbd1c39bf7d91971bf2b8c6eb78621977
kevin210310 <45213740+kevin210310@users.noreply.github.com>
2021-03-31 13:49:34 -0300

    1234

--- config/mongoose/index.js
+++ config/mongoose/index.js
@@ -1,12 +1,20 @@
 'use strict';
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/auth', {
-    useNewUrlParser: true,
+mongoose.connect(process.env.URLdb, {
+    /*useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true
+    useCreateIndex: true*/
+    useNewUrlParser: true, useUnifiedTopology: true
   }
 );
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error de Conexión a la db.'));
+db.once('open', function() {
+  console.log('Conexion a la db Mongodb exitosa');
+});
 
 module.exports = mongoose;
