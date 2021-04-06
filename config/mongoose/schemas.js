05e66057cc6f1f6c35de113cf0cb2ff985d867bd
danielsalinasjo <danielsalinasjo@gmail.com>
2021-04-06 16:42:57 -0400

    asdfas

--- config/mongoose/schemas.js
+++ config/mongoose/schemas.js
@@ -4,38 +4,33 @@ const moongose = require('mongoose');
 const Schema = moongose.Schema;
 
 const userSchema = new Schema({
-        nombre: {type: String},
-        apellidos: {type: String},
-        password: {type: String},
-        email: {type: String},
-        creado: {type: Date},
-        modificacion: {type: Boolean},
-        token: {type: String},
-        usado: {type: Boolean},
-        rol: {type: String},
-        permisos: {
-            permiso1: {type: Boolean},
-            permiso2: {type: Boolean},
-            permiso3: {type: Boolean},
-            permiso4: {type: Boolean},
-            permiso5: {type: Boolean},
-            permiso6: {type: Boolean},
-            permiso7: {type: Boolean},
-            permiso8: {type: Boolean},
-            permiso9: {type: Boolean}
-        },
-        maquinas: {
+        nombre_cliente_general: {type: String},
+        usuarios: [{
+            nombre: {type: String},
+            apellidos: {type: String},
+            password: {type: String},
+            email: {type: String},
+            creado: {type: Date},
+            modificacion: {type: Boolean},
+            token: {type: String},
+            usado: {type: Boolean},
+            rol: {type: String},
+            permisos: [{
+                permiso1: {type: Boolean}
+            }]
+        }],
+        maquinas: [{
             name: {type: String},
             status_gps: {type: Boolean},
             pos_lat: {type: Number},
             pos_lng: {type: Number},
-            dispositivos: {
+            dispositivos: [{
                 socket_name: {type: String},
                 name: {type: String},
                 pos_lat: {type: Number},
                 pos_lng: {type: Number},
                 gps: {type: Boolean},
-                sensores: {
+                sensores: [{
                     type: {type: String},
                     mode: {type: String},
                     measure: {type: String},
@@ -48,9 +43,9 @@ const userSchema = new Schema({
                     max_alarm: {type: Number}, 
                     min_alarm: {type: Number},
                     alarm: {type: Boolean}
-                }
-            }
-        }
+                }]
+            }]
+        }]
     });
 
 module.exports = moongose.model('users', userSchema);
