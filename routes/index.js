c04b2a036543071369e8caa2c7f66c3965130a2c
kevin210310 <45213740+kevin210310@users.noreply.github.com>
2021-04-13 15:20:19 -0400

    1234

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res, next) {
 });
 router.get('/login', function(req, res, next) {
   res.render('login', { 
-    title: 'Express',
     url: process.env.URL 
   });
 });
