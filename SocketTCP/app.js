07ce748de3b61c14796a037f3fc75b717b3138b3
Daniel Salinas <danielsalinasjo@gmail.com>
2021-04-22 16:49:57 -0400

    1234

--- SocketTCP/app.js
+++ SocketTCP/app.js
@@ -1,5 +1,7 @@
 require('./config/mongoose/index');
-
+let devices = require('./config/mongoose/device');
+const { now } = require('moment');
+var moment = require('moment');
 /* Or use this example tcp client written in node.js.  (Originated with 
 example code from 
 http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
@@ -9,16 +11,34 @@ var textChunk = '';
 var server = net.createServer(function(socket) {
 	socket.write('Echo server\r\n');
 	socket.on('data', function(data){
-        let data2 = Buffer.from(data, 'base64').toString('ascii');
-		console.log(data2);
-		var string = data2.split(",");
-		if(string[0] == "+BUFF:GTFRI") {
+		console.log(data);
+        //let data2 = Buffer.from(data, 'base64').toString('ascii')
+		//var string = data2.split(",");
+		
+		/*if(string[0] == "+BUFF:GTFRI") {
 			
-			console.log("NOMBRE: ", string[2]);
-			console.log("LAT: ", string[10]);
-			console.log("LNG: ", string[9]);
+			const data_gps = {
+				lat: string[12],
+				lng: string[11],
+				speed: 50.0,
+				bat: 4.23,
+				wifi: -60,
+				grade: 100,
+				status_gps: 1,
+				time: moment(Date.now()).utc().format("YYYY-MM-DD HH:mm:ss")
+			  };
+			
+			  devices.updateOne(
+				{imei: string[2] },
+				{$push: {'data': data_gps}},
+				(err, response3) => {
+				  if(err) {
+					console.log(err);
+				  }
+				}
+			  );
 
-		}
+		}*/
     	
 		socket.write(textChunk);
 	});
